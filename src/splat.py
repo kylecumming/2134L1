75b34b385aa03e92f0e4328f798852cf36712ebd
Kyle Cumming <kyle.cumming@dal.ca>
2020-01-22 14:14:46 -0400

    Added files from v3 to src

---
+++ src/splat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import splatlex
+import splatparse
+import splateval
+import sys 
+
+verbose = "-v" in sys.argv
+
+try:
+  l = splatparse.parse( sys.stdin.read() )
+  if l != None:
+    print( splateval.correct( l.eval( splateval.top_ref ) ) )
+except splatlex.ScanError as p:
+  print("{} at line {} col {} : {}".format(p.msg,p.tok.line,p.tok.col,p.tok))
+except splatparse.ParseError as p:
+  if verbose:
+    print( "Syntax error on line " + str( p.tok.line ) + ", col " + \
+          str( p.tok.col ) + " : " + str( p ) )
+  else:
+    print( "Syntax Error" )
+except splateval.EvalError as p:
+  print( "Evaluation Error while evaluating " + str( p ) + " on line " +\
+          str( p.line ) + ", column " + str( p.col ) )
